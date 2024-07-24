cd30f1bea0fc071943c42a8473540285ee4c1642
|:3c <lj2igcmkh@mozmail.com>
2024-07-24 04:56:50 +0200

    make blazingly fast

--- src/pixel_flood.cr
+++ src/pixel_flood.cr
@@ -7,49 +7,49 @@ COLORS = 6
 struct Coordinate
   property y, x
 
-  def initialize(@y : Int32, @x : Int32)
+  def initialize(@y : UInt32, @x : UInt32)
   end
 end
 
 class Area
   property neighbors
-  getter color
+  property color
   property coordinates : Array(Coordinate)
 
-  def initialize(@color : Int32, y, x)
-    @neighbors = Array(Area).new
+  def initialize(@color : UInt32, y, x)
+    @neighbors = StaticArray(Set(Area), COLORS).new { Set(Area).new }
     @coordinates = [Coordinate.new(y, x)]
   end
 
-  def merge(area : Area)
-    @neighbors.concat(area.neighbors)
+  def merge(area : Area, matrix : Array(Array(Area)))
+    area.coordinates.each do |coordinate|
+      matrix[coordinate.y][coordinate.x] = self
+    end
     @coordinates.concat(area.coordinates)
+    COLORS.times do |color|
+      @neighbors[color].concat(area.neighbors[color])
+    end
+    @neighbors[area.color].delete(area)
+    @neighbors[@color].delete(self)
   end
 
   def contact(area : Area)
     return if area == self
-    @neighbors.push area
-    area.neighbors.push self
-    @neighbors.uniq!
-    area.neighbors.uniq!
+    @neighbors[area.color].add(area)
+    area.neighbors[@color].add(self)
   end
 
-  def set_color(color : Int32, matrix : Array(Array(Area)))
-    @color = color
-    @neighbors.dup.select{ |neighbore| neighbore.color == color }.each do |neighbore|
-      self.merge(neighbore)
-      neighbore.coordinates.each do |coordinate|
-        matrix[coordinate.y][coordinate.x] = self
-      end
+  def set_color(@color, matrix : Array(Array(Area)))
+    @neighbors[@color].each do |neighbore|
+      self.merge(neighbore, matrix)
     end
-    @neighbors.uniq!
   end
 end
 
 def generate_matrix
   matrix = Array(Array(Area)).new(ROWS) do |row|
     Array(Area).new(COLUMNS) do |column|
-      Area.new(rand(1..COLORS), row, column)
+      Area.new(rand(0..(COLORS - 1)).to_u, row.to_u, column.to_u)
     end
   end
 
@@ -57,11 +57,7 @@ def generate_matrix
   loop do
     (COLUMNS - 1).times do |column|
       if matrix[row][column].color == matrix[row][column + 1].color && matrix[row][column] != matrix[row][column + 1]
-        matrix[row][column].merge(matrix[row][column + 1])
-        area = matrix[row][column]
-        matrix[row][column + 1].coordinates.each do |coordinate|
-          matrix[coordinate.y][coordinate.x] = area
-        end
+        matrix[row][column].merge(matrix[row][column + 1], matrix)
       end
     end
 
@@ -69,11 +65,7 @@ def generate_matrix
 
     COLUMNS.times do |column|
       if matrix[row][column].color == matrix[row + 1][column].color && matrix[row][column] != matrix[row + 1][column]
-        matrix[row][column].merge(matrix[row + 1][column])
-        area = matrix[row][column]
-        matrix[row + 1][column].coordinates.each do |coordinate|
-          matrix[coordinate.y][coordinate.x] = area
-        end
+        matrix[row][column].merge(matrix[row + 1][column], matrix)
       end
     end
 
@@ -98,21 +90,22 @@ def generate_matrix
   matrix
 end
 
+moves = 0
+
 NCurses.open do
-  # initialize
   NCurses.cbreak
   NCurses.noecho
   NCurses.start_color
 
-  # define background color
   pair = NCurses::ColorPair.new(1).init(NCurses::Color::WHITE, NCurses::Color::BLACK)
   
-  pairs = [NCurses::ColorPair.new(2).init(NCurses::Color::BLACK, NCurses::Color::RED    ),
-           NCurses::ColorPair.new(3).init(NCurses::Color::BLACK, NCurses::Color::BLUE   ),
-           NCurses::ColorPair.new(4).init(NCurses::Color::BLACK, NCurses::Color::GREEN  ),
+  pairs = [NCurses::ColorPair.new(2).init(NCurses::Color::BLACK, NCurses::Color::GREEN  ),
+           NCurses::ColorPair.new(3).init(NCurses::Color::BLACK, NCurses::Color::CYAN   ),
+           NCurses::ColorPair.new(4).init(NCurses::Color::BLACK, NCurses::Color::BLUE   ),
            NCurses::ColorPair.new(5).init(NCurses::Color::BLACK, NCurses::Color::MAGENTA),
-           NCurses::ColorPair.new(6).init(NCurses::Color::BLACK, NCurses::Color::YELLOW ),
-           NCurses::ColorPair.new(7).init(NCurses::Color::BLACK, NCurses::Color::CYAN   )]
+           NCurses::ColorPair.new(6).init(NCurses::Color::BLACK, NCurses::Color::RED    ),
+           NCurses::ColorPair.new(7).init(NCurses::Color::BLACK, NCurses::Color::YELLOW )]
+
 
   NCurses.bkgd(pair)
 
@@ -120,17 +113,22 @@ NCurses.open do
 
   NCurses.erase
 
+  input = -1
   loop do
     ROWS.times do |i|
       COLUMNS.times do |j|
-        NCurses.attron(pairs[matrix[i][j].color - 1].attr)
+        NCurses.attron(pairs[matrix[i][j].color].attr)
         NCurses.move(y: i, x: j * 2)
-        NCurses.addstr(" " + matrix[i][j].color.to_s)
+        NCurses.addstr(" " + (matrix[i][j].color + 1).to_s)
       end
     end
     NCurses.refresh
-    input = NCurses.getch.to_i - 48
-    break if !(1..COLORS).includes?(input)
+    old_input = input
+    input = (NCurses.getch.to_i - 49).to_u
+    break if !(0..(COLORS - 1)).includes?(input)
+    if old_input != input
+      moves += 1
+    end
     NCurses.erase
     matrix[0][0].set_color(input, matrix)
   end
@@ -138,3 +136,5 @@ NCurses.open do
   NCurses.notimeout(true)
   NCurses.getch
 end
+
+puts moves
